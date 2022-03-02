c3d1a894811b7a39b2bc8065105f11bdd031a534
oto48 <otchokhonelidze4@gmail.com>
2022-03-02 20:46:26 +0400

    add user details in form

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+  public currentStep = 0;
+  step: number = 1;
+  multiForm = new FormGroup({
+    userDetails: new FormGroup({
+      first_name: new FormControl('', Validators.required),
+      last_name: new FormControl(''),
+      phone: new FormControl(''),
+    }),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  next() {
+    this.currentStep += 1;
+    if (this.multiForm.controls['userDetails'].invalid && this.step == 1) {
+      return;
+    }
+    this.step = this.step + 1;
   }
-
 }
